de76874200f11f2fda584bc10e06fa2bb28edf5f
aso1901102 <1901102@s.asojuku.ac.jp>
2019-11-15 14:49:42 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/connecttest/MainActivity.kt
@@ -0,0 +1,15 @@
+package jp.ac.asojuku.connecttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    ///connectTest1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
